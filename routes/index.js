1d01804b1c574e56abaf02b87bb65e985386cff3
Leonardo Manuel Aguilera Romero <leoar0810@gmail.com>
2021-10-14 17:08:57 -0500

    contact added

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
 router.get('/*', function(req, res, next) {
   res.render('404', { title: 'Express' });
 });
