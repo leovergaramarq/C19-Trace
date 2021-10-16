47f3609bf09424ceff91bd99a5456b5e630d4e78
Leonardo Vergara <vergaradl@uninorte.edu.co>
2021-10-16 15:54:11 -0500

    Home sin estadísticas

--- routes/index.js
+++ routes/index.js
@@ -6,24 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('layout', {
     page: 'home',
     title: 'Home - C19-Trace',
-    // body: fs.readFileSync('./views/index.ejs', 'utf8'),
   });
 });
+
 router.get('/about', function(req, res, next) {
   res.render('layout', {
     page: 'about',
     title: 'About - C19-Trace',
-    // body: fs.readFileSync('./views/about.ejs', 'utf8'),
   });
 });
+
 router.get('/explore', function(req, res, next) {
   res.render('layout', {
     page: 'explore',
     title: 'Explore - C19-Trace',
-    // body: fs.readFileSync('./views/explore.ejs', 'utf8')
   });
 });
+
 router.get('/*', function(req, res, next) {
   res.render('404', { title: 'Express' });
 });
+
 module.exports = router;
