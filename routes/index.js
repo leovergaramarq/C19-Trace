aa8e71c330caf032c57610d2b532e85cd010a938
Leonardo Vergara <vergaradl@uninorte.edu.co>
2021-10-16 15:28:54 -0500

    Home fase beta

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('layout', {
+    page: 'home',
+    title: 'Home - C19-Trace',
+    // body: fs.readFileSync('./views/index.ejs', 'utf8'),
+  });
 });
-router.get('/contact', function(req, res, next) {
-  res.render('contact');
+router.get('/about', function(req, res, next) {
+  res.render('layout', {
+    page: 'about',
+    title: 'About - C19-Trace',
+    // body: fs.readFileSync('./views/about.ejs', 'utf8'),
+  });
+});
+router.get('/explore', function(req, res, next) {
+  res.render('layout', {
+    page: 'explore',
+    title: 'Explore - C19-Trace',
+    // body: fs.readFileSync('./views/explore.ejs', 'utf8')
+  });
 });
 router.get('/*', function(req, res, next) {
   res.render('404', { title: 'Express' });
