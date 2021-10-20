042a7b5d4d52d737c23ffc35f17f5d8dd6a80e8a
Leonardo Vergara <vergaradl@uninorte.edu.co>
2021-10-19 21:59:05 -0500

    Api actualizada

--- helpers/numeric.js
+++ helpers/numeric.js
@@ -1,8 +1,7 @@
-
 function isInt(number){
     try{
-        parseInt(number);
-        return number !==NaN;
+        number = parseInt(number);
+        return !isNaN(number);
     }catch(err){
         return false;
     }
